27af69a4f71d7cb63f97d06694631d5c5dee72da
Jake Guida <jlguida@umail.ucsb.edu>
2018-12-09 19:53:03 -0800

    added login screen, UCSB REST api now working

--- app/src/main/java/com/jakebliss/gauchoassistant/gauchoassistant/LoginActivity.java
+++ app/src/main/java/com/jakebliss/gauchoassistant/gauchoassistant/LoginActivity.java
@@ -2,10 +2,16 @@ package com.jakebliss.gauchoassistant.gauchoassistant;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -14,13 +20,23 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_login);
-        Button b = findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(LoginActivity.this, WidgetActivity.class);
-                LoginActivity.this.startActivity(myIntent);
-            }
-        });
+        Typeface tf = Typeface.createFromAsset(getAssets(),"product_sans_regular.ttf");
+        TextView textfield = findViewById(R.id.general2);
+        textfield.setTypeface(tf,Typeface.NORMAL);
+        textfield = findViewById(R.id.editText);
+        textfield.setTypeface(tf,Typeface.NORMAL);
+        textfield = findViewById(R.id.editText2);
+        textfield.setTypeface(tf,Typeface.NORMAL);
+        textfield = findViewById(R.id.textView2);
+        textfield.setTypeface(tf,Typeface.NORMAL);
+        textfield = findViewById(R.id.button);
+        textfield.setTypeface(tf,Typeface.NORMAL);
     }
 }
--- app/src/main/java/com/jakebliss/gauchoassistant/gauchoassistant/chat/ChatActivity.java
+++ app/src/main/java/com/jakebliss/gauchoassistant/gauchoassistant/chat/ChatActivity.java
@@ -3,12 +3,19 @@ package com.jakebliss.gauchoassistant.gauchoassistant.chat;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.FragmentTransaction;
+import java.util.Calendar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.util.JsonReader;
+import android.util.JsonToken;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
@@ -21,6 +28,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -28,6 +37,10 @@ import io.reactivex.disposables.CompositeDisposable;
 
 public class ChatActivity extends AppCompatActivity {
     private Toolbar mToolbar;
+    private String quarter_calendar = new String();
+    private String menu = new String();
+    private String quarter_calendar_data = new String("quarter_calendar");
+    private String menu_data = new String("menu");
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, ChatActivity.class);
@@ -46,43 +59,76 @@ public class ChatActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.parseColor("#232323"));
         }
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = "Current Date : " + mdformat.format(calendar.getTime());
+        strDate += "T00:00:00-08:00";
+        Log.d("api", strDate);
+        //Make REST API calls to the UCSB dev apis
+        getResponseData("https://api.ucsb.edu/academics/quartercalendar/v1/quarters/current", quarter_calendar_data);
+        getResponseData("https://api.ucsb.edu/dining/menu/v1/2018-12-09T00%3A00%3A00-08%3A00/de-la-guerra/brunch", menu_data);
+
+        Log.d("api", quarter_calendar);
+        bindViews();
+        init();
+    }
+
+    private String getResponseData(final String endpoint, final String data_type){
+        String data = new String("null");
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                // All your networking logic
-                // should be here
-                // Create URL
                 URL githubEndpoint = null;
                 try {
-                    githubEndpoint = new URL("https://api.github.com/");
+                    githubEndpoint = new URL(endpoint);
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
-
-// Create connection
                 try {
                     HttpsURLConnection myConnection =
                             (HttpsURLConnection) githubEndpoint.openConnection();
-                    myConnection.setRequestProperty("User-Agent", "gaucho-assistant-app");
+                    myConnection.setRequestProperty("User-Agent", "gaucho-assistant");
+                    myConnection.setRequestProperty("accept",
+                            "application/json");
+                    myConnection.setRequestProperty("ucsb-api-version",
+                            "1.0");
+                    myConnection.setRequestProperty("ucsb-api-key",
+                            "8IQgXsvmA6OuIjuGkVT3t9oYf2MDZbbS");
                     if (myConnection.getResponseCode() == 200) {
                         Log.d("api", "gotit");
                         InputStream responseBody = myConnection.getInputStream();
                         InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
                         JsonReader jsonReader = new JsonReader(responseBodyReader);
-                        Log.d("api", jsonReader.toString());
-                        jsonReader.beginObject(); // Start processing the JSON object
-                        while (jsonReader.hasNext()) { // Loop through all keys
-                            String key = jsonReader.nextName(); // Fetch the next key
-                            if (key.equals("organization_url")) { // Check if desired key
-                                // Fetch the value as a String
-                                String value = jsonReader.nextString();
+                        List<Object> obj = new ArrayList<Object>();
 
-                                // Do something with the value
-                                // ...
-                                Log.d("api", value);
-                                break; // Break out of the loop
-                            } else {
-                                jsonReader.skipValue(); // Skip values of other keys
+                        if(data_type.equals("menu")){
+                            jsonReader.beginArray();
+                            while(jsonReader.hasNext()){
+                                jsonReader.beginObject(); // Start processing the JSON object
+                                while (jsonReader.hasNext()) {
+                                    String key = jsonReader.nextName(); // Fetch the next key
+                                    if (jsonReader.peek() != JsonToken.NULL) {
+                                            menu_data += "," + key + ":" + jsonReader.nextString();
+                                            Log.d("api", menu_data);
+                                    } else {
+                                        jsonReader.nextNull(); // note on this
+                                    }
+                                }
+                                jsonReader.endObject();
+                            }
+                            jsonReader.endArray();
+                        }
+                        else {
+                            jsonReader.beginObject(); // Start processing the JSON object
+                            while (jsonReader.hasNext()) { // Loop through all keys
+                                String key = jsonReader.nextName(); // Fetch the next key
+                                // Check if desired key
+                                if (jsonReader.peek() != JsonToken.NULL) {
+                                        quarter_calendar += "," + key + ":" + jsonReader.nextString();
+                                        Log.d("api", quarter_calendar);
+                                } else {
+                                    jsonReader.nextNull(); // note on this
+                                }
                             }
                         }
                         jsonReader.close();
@@ -96,8 +142,7 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
-        bindViews();
-        init();
+        return data;
     }
 
 
