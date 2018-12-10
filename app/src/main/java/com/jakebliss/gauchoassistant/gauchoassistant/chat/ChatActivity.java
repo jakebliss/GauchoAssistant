85a499f6cef54c920c764651da03b736b8782d12
Jake Guida <jlguida@umail.ucsb.edu>
2018-12-09 16:48:13 -0800

    initial REST api set up

--- app/src/main/java/com/jakebliss/gauchoassistant/gauchoassistant/chat/ChatActivity.java
+++ app/src/main/java/com/jakebliss/gauchoassistant/gauchoassistant/chat/ChatActivity.java
@@ -3,15 +3,28 @@ package com.jakebliss.gauchoassistant.gauchoassistant.chat;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toolbar;
 
 import com.jakebliss.gauchoassistant.gauchoassistant.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import io.reactivex.disposables.CompositeDisposable;
 
 public class ChatActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -25,6 +38,7 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        final CompositeDisposable compositeDisposable = new CompositeDisposable();
         //hide action bar
         getSupportActionBar().hide();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -32,6 +46,56 @@ public class ChatActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.parseColor("#232323"));
         }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // All your networking logic
+                // should be here
+                // Create URL
+                URL githubEndpoint = null;
+                try {
+                    githubEndpoint = new URL("https://api.github.com/");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+// Create connection
+                try {
+                    HttpsURLConnection myConnection =
+                            (HttpsURLConnection) githubEndpoint.openConnection();
+                    myConnection.setRequestProperty("User-Agent", "gaucho-assistant-app");
+                    if (myConnection.getResponseCode() == 200) {
+                        Log.d("api", "gotit");
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+                        Log.d("api", jsonReader.toString());
+                        jsonReader.beginObject(); // Start processing the JSON object
+                        while (jsonReader.hasNext()) { // Loop through all keys
+                            String key = jsonReader.nextName(); // Fetch the next key
+                            if (key.equals("organization_url")) { // Check if desired key
+                                // Fetch the value as a String
+                                String value = jsonReader.nextString();
+
+                                // Do something with the value
+                                // ...
+                                Log.d("api", value);
+                                break; // Break out of the loop
+                            } else {
+                                jsonReader.skipValue(); // Skip values of other keys
+                            }
+                        }
+                        jsonReader.close();
+                        myConnection.disconnect();
+                    } else {
+                        // Error handling code goes here
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
         bindViews();
         init();
     }
